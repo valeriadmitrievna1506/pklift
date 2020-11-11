f4f59ecfff7a12a32b567a82626772da5dbd5372
valeriadmitrievna1506 <valeriadmitrievna1506@gmail.com>
2020-11-11 17:15:01 +0300

    scroll up + header/footer with links

---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+let tabs = {
+    'autos-nav': 'autos',
+    'lifts-nav': 'lifts',
+    'warehouse-nav': 'warehouse',
+    'workers-nav': 'workers',
+};
+
+$('.gallery .container nav p').click(function (event) {
+    let navTabs = document.querySelectorAll('.gallery .container nav p');
+    navTabs.forEach(element => {
+        if (element.classList == 'focused') {
+            element.classList = '';
+            $('#' + tabs[$(element).attr('id')]).removeClass('focused-block')
+        }
+    });
+    event.target.classList = 'focused';
+    $('#' + tabs[$(event.target).attr('id')]).addClass('focused-block');
+});
--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 $('#input-tel').mask('+7 (999) 999 99 99');
+
+$(function() {
+    $('.scrollup').click(function() {
+      $("html, body").animate({
+        scrollTop:0
+      },600);
+    })
+  })
